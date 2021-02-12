7d804d1618baac41c44395bb2812da73e4db83b5
pechenkin <pechenkin@bpcbt.com>
2021-02-12 16:44:52 +0300

    * refactor dao service

---
+++ src/main/java/com/myworktechs/statcounter/service/StatisticsDao.java
@@ -0,0 +1,50 @@
+package com.myworktechs.statcounter.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class StatisticsDao {
+
+    public static final String MERGE_ITEM = "MERGE INTO COUNTRY_STATISTICS KEY (COUNTRY) VALUES (:country, :counter)";
+    public static final String DELETE_ALL = "DELETE FROM COUNTRY_STATISTICS";
+    public static final String SELECT_VALUES = "SELECT COUNTRY, COUNTER FROM COUNTRY_STATISTICS";
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Autowired
+    public StatisticsDao(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    public void saveItem(long counterForCountry, String country) {
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("country", country);
+        parameterSource.addValue("counter", counterForCountry);
+
+        namedParameterJdbcTemplate.update(MERGE_ITEM, parameterSource);
+    }
+
+    public void clear() {
+        namedParameterJdbcTemplate.update(DELETE_ALL, Collections.emptyMap());
+    }
+
+    public Map<String, AtomicLong> getInitialValues() {
+        return namedParameterJdbcTemplate.query(SELECT_VALUES, rs -> {
+            Map<String, AtomicLong> partnersMap = new HashMap<>();
+            while (rs.next()) {
+                String country = rs.getString("COUNTRY");
+                long count = rs.getLong("COUNTER");
+                partnersMap.put(country, new AtomicLong(count));
+            }
+            return partnersMap;
+        });
+    }
+}
--- src/main/java/com/myworktechs/statcounter/service/StatisticsService.java
+++ src/main/java/com/myworktechs/statcounter/service/StatisticsService.java
@@ -1,12 +1,10 @@
 package com.myworktechs.statcounter.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.*;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
@@ -15,25 +13,17 @@ import java.util.stream.Collectors;
 @Service
 public class StatisticsService {
 
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final StatisticsDao statisticsDao;
     private final ConcurrentMap<String, AtomicLong> statisticsMap = new ConcurrentHashMap<>();
 
     @Autowired
-    public StatisticsService(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    public StatisticsService(StatisticsDao statisticsDao) {
+        this.statisticsDao = statisticsDao;
     }
 
     @PostConstruct
     private void init() {
-        Map<String, AtomicLong> map = namedParameterJdbcTemplate.query("SELECT COUNTRY, COUNTER FROM COUNTRY_STATISTICS", rs -> {
-            Map<String, AtomicLong> partnersMap = new HashMap<>();
-            while (rs.next()) {
-                String country = rs.getString("COUNTRY");
-                long count = rs.getLong("COUNTER");
-                partnersMap.put(country, new AtomicLong(count));
-            }
-            return partnersMap;
-        });
+        Map<String, AtomicLong> map = statisticsDao.getInitialValues();
         statisticsMap.putAll(map);
     }
 
@@ -46,11 +36,7 @@ public class StatisticsService {
         }
         long counterForCountry = atomicLong.incrementAndGet();
 
-        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
-        parameterSource.addValue("country", country);
-        parameterSource.addValue("counter", counterForCountry);
-
-        namedParameterJdbcTemplate.update("MERGE INTO COUNTRY_STATISTICS KEY (COUNTRY) VALUES (:country, :counter)", parameterSource);
+        statisticsDao.saveItem(counterForCountry, country);
     }
 
     public Map<String, Long> report() {
@@ -63,7 +49,7 @@ public class StatisticsService {
      */
     public void reset() {
         statisticsMap.clear();
-        namedParameterJdbcTemplate.update("DELETE FROM COUNTRY_STATISTICS", Collections.emptyMap());
+        statisticsDao.clear();
 
     }
 }
